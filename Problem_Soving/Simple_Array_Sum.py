8809bd9d1aa7ac83769b30c53fbb68b0e86b0072
Sabin Ghimire <41796281+serrotsabin@users.noreply.github.com>
2019-02-25 12:15:45 +0545

    Add files via upload

---
+++ Problem_Soving/Simple_Array_Sum.py
@@ -0,0 +1,17 @@
+import os
+import sys
+def simpleArraySum(ar):
+    result = sum(ar)
+    return(result)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
