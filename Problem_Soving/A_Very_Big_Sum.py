f4cb8563796ba7163be4dd0aa6b57d6dd975e64c
Sabin Ghimire <41796281+serrotsabin@users.noreply.github.com>
2019-03-07 15:34:40 +0545

    Add files via upload

---
+++ Problem_Soving/A_Very_Big_Sum.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the aVeryBigSum function below.
+#
+def aVeryBigSum(n, ar):
+    #
+    # Write your code here.
+    #
+    s = 0
+    for i in range(n):
+        s = s + ar[i]
+    return(s)
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(n, ar)
+
+    f.write(str(result) + '\n')
+
+    f.close()
