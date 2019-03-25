1ab5e1f9f839371d9e9842410ab2f63d54b303c5
Sabin Ghimire <41796281+serrotsabin@users.noreply.github.com>
2019-03-26 00:04:49 +0545

    Add files via upload

---
+++ Problem_Soving/Between_Two_Sets.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the countApplesAndOranges function below.
+#
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    #
+    # Write your code here.
+    #
+    app = 0
+    ora = 0
+    for i in apple:
+        lo = a+i
+        if lo >= s and lo <= t:
+            app += 1
+    for i in orange:
+        lo = b+i
+        if lo >= s and lo <= t:
+            ora += 1
+    print(app)
+    print(ora)
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apple = list(map(int, input().rstrip().split()))
+
+    orange = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apple, orange)
